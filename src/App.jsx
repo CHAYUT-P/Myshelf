50bea75d43d6f2a22b0d8bc7fd57f63e41443a01
Chayut Panangkasiri <chayut@chayuts-MacBook-Pro.local>
2025-08-16 17:44:16 +0700

    sidebar-tab

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Leftbar from './components/Leftbar/Sidebar'
-import Topbar from './components/Topbar/Topbar'
+import { useState } from "react";
+import "./App.css";
+import Sidebar from "./components/Leftbar/Sidebar";
+import Topbar from "./components/Topbar/Topbar";
 
 function App() {
   const [isLeftbarOpen, setIsLeftbarOpen] = useState(false);
+  const [shelves, setShelves] = useState([{ id: 1, name: "Shelf 1" }]);
+  const [activeShelf, setActiveShelf] = useState(1);
+
+  const handleAddShelf = () => {
+    let newId;
+
+    if (shelves.length > 0) {
+      newId = shelves[shelves.length - 1].id + 1;
+    } else {
+      newId = 1;
+    }
+
+    const newShelf = { id: newId, name: `Shelf ${newId}` };
+    const updatedShelves = [...shelves, newShelf];
+
+    setShelves(updatedShelves);
+    setActiveShelf(newId);
+  };
+
+  const handleSelectShelf = (id) => {
+    setActiveShelf(id);
+  };
 
   const toggleLeftbar = () => {
     setIsLeftbarOpen(!isLeftbarOpen);
   };
 
+  // Find current shelf
+  const nowActiveShelf = shelves.find((s) => s.id === activeShelf) || null;
+
   return (
     <div>
       <Topbar isOpen={isLeftbarOpen} toggleOpen={toggleLeftbar} />
-      <Leftbar isOpen={isLeftbarOpen} />
-      {/* other content */}
+      <Sidebar
+        isOpen={isLeftbarOpen}
+        shelves={shelves}
+        activeShelfId={activeShelf}
+        onSelectShelf={handleSelectShelf}
+        onAddShelf={handleAddShelf}
+      />
     </div>
   );
 }
 
-
-export default App
+export default App;
--- src/components/Leftbar/Sidebar.jsx
+++ src/components/Leftbar/Sidebar.jsx
@@ -2,26 +2,48 @@ import React from "react";
 import "./Sidebar.css";
 import PlusButton from "./plus-large-svgrepo-com.svg?react";
 
-export default function Sidebar({ isOpen }) {
+export default function Sidebar({
+  isOpen,
+  shelves,
+  activeShelfId,
+  onSelectShelf,
+  onAddShelf
+}) {
   return (
     <div className={`sidebar ${isOpen ? "show" : "hide"}`}>
+      {/* Favourite Section */}
       <div className="fav-sidebar">
         <div className="fav-header">
           <span className="fav-text">Favourite</span>
           <ul className="fav-list"></ul>
         </div>
-        <div className="fav-items"></div>
       </div>
 
+      {/* All Shelves Section */}
       <div className="all-shelves-sidebar">
         <div className="all-shelves-header">
           <span className="all-shelves-text">All Shelves</span>
-          <button className="add-btn" id="add-shelf">
+          <button
+            className="add-btn"
+            id="add-shelf"
+            onClick={onAddShelf} // ✅ call prop
+          >
             <PlusButton className="add-icon" />
           </button>
-          <ul className="shelves-list"></ul>
         </div>
-        <div className="all-shelves-items"></div>
+
+        {/* Shelf list */}
+        <ul className="shelves-list">
+          {shelves.map((shelf) => (
+            <li
+              key={shelf.id}
+              className={`shelf-item ${shelf.id === activeShelfId ? "active" : ""}`}
+              onClick={() => onSelectShelf(shelf.id)}
+            >
+              {shelf.name}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
