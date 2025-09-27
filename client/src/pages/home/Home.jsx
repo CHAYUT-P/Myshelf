bef302673d9bddf36c20002b5c5d94c17b8c078f
Chayut Panangkasiri <chayut@chayuts-MacBook-Pro.local>
2025-09-28 00:05:09 +0700

    register login pages login still no function

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react";
+import "./Home.css";
+
+
+function Home() {
+
+    return(
+    <div>
+        <nav>
+            <a></a>
+            <a></a>
+            <a></a>
+            <a></a>
+        </nav>
+    </div>)
+}
+
+export default Home;
